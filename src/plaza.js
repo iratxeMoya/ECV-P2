9e7e1b6a63be68321434af24d1b9a88c705bb4d8
iratxeMoya <pyros@template-d7.upf.ed>
2020-01-29 21:30:37 +0100

    hey

--- src/plaza.js
+++ src/plaza.js
@@ -1,7 +1,11 @@
-var io = require('socket.io-client');
+
+import io from 'socket.io';
 var mysocket = io();
+console.log(document.querySelector("div"));
+var button = document.querySelector("button.send");
+button.addEventListener("click", send_message);
 
 function send_message(){
-	console.lo("message sent");
+	console.log("message sent");
 	mysocket.emit('message',"HOASKDJAS");
 }
