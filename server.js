40b7ba055029c31589786b596b617d4eb9f5634b
Iratxe Moya <imoya@BCNSDWW19IMOYA.scytl.net>
2020-01-30 11:39:48 +0100

    a

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var wss = new WebSocket.Server({server});
 		ws.on('message', data => {
 			console.log('data: ', data);
 			wss.clients.forEach(client => {
+				console.log('client: ', client)
 				if(client.readyState === WebSocket.OPEN) {
 					client.send(data);
 				}
@@ -22,23 +23,3 @@ server.listen(9027, function() {
 	console.log('app listening on port 9027');
 })
 app.use(express.static(__dirname + '/src'));
-
-/*app.get('/',(req,res)=>{
-	console.log('in get');
-	res.send('index.html');
-})
-
-server.listen(9027,function(){
-	console.log("listening to port 9027");
-})
-
-io.on('connection', () => {
-	console.log('user connected');
-});
-
-io.on('message', function(message) {
-	console.log(message);
-}); */
-
-
-
