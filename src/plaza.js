3cce02f0037d9fb67c9717a882a19c9369215aab
Pau Pamos <pau.pamos01@estudiant.upf.edu>
2020-02-02 10:17:13 +0100

    tile movement

--- src/plaza.js
+++ src/plaza.js
@@ -156,40 +156,65 @@ function on_key_press_send_msg(event) {
 
 //page setup
 const tilesize = 50;
+const bordersize = 2;
+var h=0;
+var mapdim=0;
+var i =0;
+var j=0;
+var style;
 
-var h = document.getElementById("main_plaza").style.height;
-var w = document.getElementById("main_plaza").style.width;
+style = window.getComputedStyle(document.getElementById("main_plaza"));
+console.log(document.getElementById("main_plaza").style.border);
+h = style.height;
+h=parseInt(h.substring(0, h.length - 2));
 
-console.log(document.getElementById("main_plaza").style);
 if(h%tilesize!=0 || w%tilesize!=0){
 	h=Math.floor(h/tilesize)*tilesize;
-	document.getElementById("main_plaza").style.height = h;
-	document.getElementById("main_plaza").style.width = h;
+	document.getElementById("main_plaza").style.height = h+"px";
+	document.getElementById("main_plaza").style.width = h+"px";
 }
 
-var mapdim = Math.floor(h/tilesize);
+mapdim = Math.floor(h/tilesize);
 
-console.log(mapdim)
+for (i=0;i<mapdim;i++){
+	for (j=0;j<mapdim;j++){
+		let e =document.createElement("span");
+		e.classList.add("tile");
+		e.style.top=(i*tilesize)+"px";
+		e.style.left=(j*tilesize)+"px";
+		e.style.zIndex =0;
+		document.getElementById("main_plaza").appendChild(e);
+	}
+}
 
 //movement 
-var posx=0;
-var posy=0;
-var finalx=0;
-var finaly=0
+var posx=bordersize;
+var posy=bordersize;
+var finalx=bordersize;
+var finaly=bordersize;
 var movingx = false;
 var movingy	=false;
 var gridposx=0;
 var gridposy=0;
+var placed_obj =false;
+var obj_counter=0;
+var objects=[];
 var updating_func = setInterval(update, 50);
-var map = Array(mapdim).fill(0).map(()=>Array(mapdim).fill(0))
+var map = Array(mapdim).fill(0).map(()=>Array(mapdim).fill(0));
 
-console.log(map);
 
 document.body.addEventListener("click",function(event){
-	finalx = Math.floor(event.clientX/tilesize)*tilesize;
-	finaly = Math.floor(event.clientY/tilesize)*tilesize;
-	movingx = true;
-	movingy = true;
+	finalx = Math.floor(event.clientX/tilesize)*tilesize+bordersize;
+	if(finalx>(mapdim-1)*tilesize){
+		finalx=(mapdim-1)*tilesize+bordersize;
+	}
+	finaly = Math.floor(event.clientY/tilesize)*tilesize+bordersize;
+	if(finaly>(mapdim-1)*tilesize){
+		finaly=(mapdim-1)*tilesize+bordersize;
+	}
+	if(!movingx){
+		movingy = true;
+	}
 });
 
 
@@ -198,13 +223,50 @@ function rotate_vec(x,y){
 }
 
 function update(){
+	let prob = Math.random()*100;
+	let rx = 0;
+	let ry = 0;
+	let newelement;
+	let i=0;
+	if(prob>90 && !placed_obj){
+		while (rx==gridposx && ry==gridposy){
+			rx=Math.floor(Math.random()*(mapdim));
+			ry=Math.floor(Math.random()*(mapdim));
+			console.log(rx+" "+ry); 
+		}
+		newelement = document.createElement("span");
+		newelement.classList.add("tile");
+		newelement.id="obj";
+		newelement.style.backgroundColor="yellow";
+		newelement.style.top=ry*tilesize+"px";
+		newelement.style.left=rx*tilesize+"px";
+		document.getElementById("main_plaza").appendChild(newelement);
+		objects.push([rx,ry]);
+		placed_obj=true;
+	}
+	
+	if(placed_obj){
+		for(i=0;i<objects.length;i++){
+			if(objects[i][0]==gridposx && objects[i][1]==gridposy){
+				obj_counter++;
+				console.log(document.getElementById("obj"));
+				document.getElementById("main_plaza").removeChild(document.getElementById("obj"));
+				objects.pop();
+				placed_obj=false;
+			}
+		}
+	}
+	
 	if(movingy){
 		if(posy<finaly){
 			posy+=10;
 		}else if (posy>finaly){
 			posy-=10;
 		}
-		if(posy==finaly){
+		if(posy==finaly && posx==finalx){
+			movingy=false;
+			movingx=false;
+		}else if(posy==finaly){
 			movingy=false;
 			movingx=true;
 		}
@@ -215,7 +277,7 @@ function update(){
 			posx-=10;
 		}
 		if(posx==finalx){
-			movingx=false;
+			movingy=true;
 		}
 	}
 	
@@ -226,8 +288,8 @@ function update(){
 	
 	map[gridposx][gridposy]=1;
 
-	document.getElementsByClassName("pj2")[0].style.top = gridposy+"px";
-	document.getElementsByClassName("pj2")[0].style.left = gridposx+"px";
+	document.getElementsByClassName("pj2")[0].style.top = (tilesize*gridposy)+"px";
+	document.getElementsByClassName("pj2")[0].style.left = (tilesize*gridposx)+"px";
 	
 	document.getElementsByClassName("pj")[0].style.top = posy+"px";
 	document.getElementsByClassName("pj")[0].style.left = posx+"px";
