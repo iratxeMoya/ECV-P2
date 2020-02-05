360139deb6ec0fae20c6cc010d33164e06f64796
iratxeMoya <iratxe.moya@gmail.com>
2020-02-05 16:36:07 +0100

    test

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ wss.on('connection', function(ws) {
 	ws.on('message', function(data){
 
 		var jsonData = JSON.parse(data);
-		console.log('new message in server: ', jsonData);
+		console.log('new message in server: ', jsonData.type);
 
 		if(jsonData.type === 'login') {
 
