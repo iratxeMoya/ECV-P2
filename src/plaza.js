2ecf77e010819bc458f13a48f763ed52a1360430
iratxeMoya <iratxe.moya@gmail.com>
2020-02-05 19:09:28 +0100

    no se como ira esto

--- src/plaza.js
+++ src/plaza.js
@@ -369,3 +369,16 @@ function onKeyDownChangePass (event) {
     }
 }
 
+
+//UTILS
+
+Array.prototype.delete = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
