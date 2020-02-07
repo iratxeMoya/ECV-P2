9072c05c9816991f8eb708653f8a46f985ad64a3
iratxeMoya <iratxe.moya@gmail.com>
2020-02-07 16:16:47 +0100

    test

--- server.js
+++ server.js
@@ -180,7 +180,7 @@ function broadcastMsg(data, onlyNear, x, y) {
 
 	connectedClients.forEach(client => {
 		if (onlyNear) {
-			if (client.actualPosition_x - x <= 20 && client.actualPosition_y - y <= 20) {
+			if (client.actualPosition_x - x <= 4 || client.actualPosition_y - y <= 4) {
 				client.connection.send(data);
 			}
 		} else {
